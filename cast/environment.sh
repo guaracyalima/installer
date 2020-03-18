4d6a7cfd7790df24949cae8ac9b0df4effb0971d
guaracyalima <guaracyaraujolima@gmail.com>
2020-03-18 13:38:25 -0300

    update node installation

--- cast/environment.sh
+++ cast/environment.sh
@@ -108,7 +108,9 @@ echo -e "\n\n\n Iniciando instalação do NodeJS \n\n";
 
 curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.35.3/install.sh | bash
 
-export NVM_DIR="$([ -z "${XDG_CONFIG_HOME-}" ] && printf %s "${HOME}/.nvm" || printf %s "${XDG_CONFIG_HOME}/nvm")" [ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"
+cat ./bashrc | sudo tee -a ~/.bashrc
+
+source ~/.bashrc
 
 nvm install --lts
 
